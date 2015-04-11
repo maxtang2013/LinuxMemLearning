6dbe2eb1dfc01440c61ae202fce161758a2278b0
Max Tang <yllfever@gmail.com>
2015-04-11 04:27:52 -0700

    Implimented a function to list all permutation of (1 2 3 4).

--- a.c
+++ a.c
@@ -5,3 +5,31 @@ void printSomething()
     printf("That's it\n");
 }
 
+#define MAX_PERM_SIZE 8
+static int a[MAX_PERM_SIZE] = {0};
+static int usd[MAX_PERM_SIZE] = {0};
+void allPerm(int n, int i)
+{
+    int j = 0;
+    if (i == 0)
+    {
+        for (j = n; j >= 1; --j)
+        {
+            printf("%2d", a[j]);
+        }
+        printf("\n");
+        return;
+    }
+
+    for (j = 1; j <= n; ++j)
+    {
+        if (!usd[j])
+        {
+            usd[j] = 1;
+            a[i] = j;
+            allPerm(n, i - 1);
+            usd[j] = 0;
+        }
+    }
+}
+
--- main.c
+++ main.c
@@ -1,9 +1,11 @@
 
 void printSomething();
+void allPerm(int n, int i);
 
 int main()
 {
     printSomething();
+    allPerm(4, 4);
     return 0;
 }
 
